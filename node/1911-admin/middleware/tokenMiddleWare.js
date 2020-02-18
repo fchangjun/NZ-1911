14e4184e981f95d9430d1ff810b5b51c609e69ff
Apple <apple@AppledeMacBook-Pro.local>
2020-02-18 11:54:05 +0800

    node 07-am

--- node/1911-admin/middleware/tokenMiddleWare.js
+++ node/1911-admin/middleware/tokenMiddleWare.js
@@ -1,6 +1,11 @@
+const {verifyToken} = require('../utils/jwt')
 let tokenMiddlWare = (req,res,next)=>{
-  console.log('token拦截器')
-  let tokenState = false 
+  console.log('token拦截器',req.body)
+  let {token} = req.body 
+  if(!token){return res.send({err:-997,msg:'token丢失'})} 
+  //获取验证token的状态
+  let tokenState = verifyToken(token)
+  console.log(tokenState) 
   if(tokenState){
     next()
   }else{
--- node/1911-admin/utils/jwt.js
+++ node/1911-admin/utils/jwt.js
@@ -1,11 +1,32 @@
 const JWT = require('jsonwebtoken')
 // 对jwt相关的封装
 const secret = 'shdfkjshfjkshfk131313'
-let  createToken =()=>{
-  let token =JWT.sign({us:123},secret)
+let  createToken =(data,expiresIn)=>{
+  let  obj ={} 
+  obj.data=data||{}  //存入token的数据
+  obj.ctime =(new Date()).getTime() //token的创建时间
+  obj.expiresIn = expiresIn||1000*60*60*24*7 //设定的过期时间
+  let token =JWT.sign(obj,secret)
   return token
+}
+
+let verifyToken=(token)=>{
+  let result = null 
+  try {
+     let {data,ctime,expiresIn} = JWT.verify(token,secret)
+     //判断token的过期时间  
+     let nowTime =(new Date()).getTime()
+     if(nowTime-ctime<expiresIn){
+      //  没有过期
+       result = data 
+     }
+     
+  } catch (error) {  
+  }
+  console.log(result)
+  return result
 }
 
 
 
-module.exports={createToken}
+module.exports={createToken,verifyToken}
