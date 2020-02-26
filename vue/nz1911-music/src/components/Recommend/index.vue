2532c07cc055a6790fd2da35432819eed54d55a8
Apple <apple@AppledeMacBook-Pro.local>
2020-02-26 16:59:54 +0800

    vue05-pm

--- vue/nz1911-music/src/components/Recommend/index.vue
+++ vue/nz1911-music/src/components/Recommend/index.vue
@@ -1,51 +1,102 @@
 <template>
   <div class='recommend'>
-    <banner :banners='list'></banner>
+  <!-- 滚动区域 -->
+    <div class='wrapper' ref='wrapper'>
+      <div class='content'>
+        <!-- 轮播图 -->
+        <banner :banners='list'></banner>
+        <!-- 歌曲列表 -->
+        <div class='title'>歌曲推荐列表</div>
+        <ul class='list'>
+          <li v-for='(item,index) in recommendList' 
+              :key = "index"
+          >
+             <div class ='left'>
+               <!-- <img :src="item.imgurl" alt=""> -->
+                <img v-lazy="item.imgurl" alt="">
+             </div>
+             <div class='right'>
+                <p class='name'>{{item.creator.name}}</p>
+                <p class='album'>{{item.dissname}}</p>
+             </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import Banner  from '../Banner'
+import {getBannerData,getRecommendData} from '../../api/api'
 // import axios from '../../utils/axios'
+import BS from  'better-scroll'
 export default {
   components:{Banner},
   data(){
     return{
-      list:[]
+      list:[],
+      recommendList:[]
     }
   },
   methods:{
-    getBannerData(){
-     //获取轮播图数据
-     let url ='/hehe/music/api/getTopBanner?g_tk=1928093487&inCharset=utf8&outCharset=utf-8&notice=0&format=json&platform=yqq.json&hostUin=0&needNewCode=0&-=recom012845016368453477&data=%7B%22comm%22:%7B%22ct%22:24%7D,%22category%22:%7B%22method%22:%22get_hot_category%22,%22param%22:%7B%22qq%22:%22%22%7D,%22module%22:%22music.web_category_svr%22%7D,%22recomPlaylist%22:%7B%22method%22:%22get_hot_recommend%22,%22param%22:%7B%22async%22:1,%22cmd%22:2%7D,%22module%22:%22playlist.HotRecommendServer%22%7D,%22playlist%22:%7B%22method%22:%22get_playlist_by_category%22,%22param%22:%7B%22id%22:8,%22curPage%22:1,%22size%22:40,%22order%22:5,%22titleid%22:8%7D,%22module%22:%22playlist.PlayListPlazaServer%22%7D,%22new_song%22:%7B%22module%22:%22newsong.NewSongServer%22,%22method%22:%22get_new_song_info%22,%22param%22:%7B%22type%22:5%7D%7D,%22new_album%22:%7B%22module%22:%22newalbum.NewAlbumServer%22,%22method%22:%22get_new_album_info%22,%22param%22:%7B%22area%22:1,%22sin%22:0,%22num%22:10%7D%7D,%22new_album_tag%22:%7B%22module%22:%22newalbum.NewAlbumServer%22,%22method%22:%22get_new_album_area%22,%22param%22:%7B%7D%7D,%22toplist%22:%7B%22module%22:%22musicToplist.ToplistInfoServer%22,%22method%22:%22GetAll%22,%22param%22:%7B%7D%7D,%22focus%22:%7B%22module%22:%22QQMusic.MusichallServer%22,%22method%22:%22GetFocus%22,%22param%22:%7B%7D%7D%7D'
-     this.$axios.get(url)
-    .then((res)=>{
-      console.log(res)
-      this.list = res.data.slider
-    })
-    },
-    getRecommendData(){
-      // 获取推荐页面的歌曲数据
-      let url ='http://ustbhuangyi.com/music/api/getDiscList?g_tk=1928093487&inCharset=utf-8&outCharset=utf-8&notice=0&format=json&platform=yqq&hostUin=0&sin=0&ein=29&sortId=5&needNewCode=0&categoryId=10000000&rnd=0.3983624931217431'
-      this.$axios.get(url).then((res)=>{
-        console.log(res)
-      })
+    initBs(){
+      let wrapper = this.$refs.wrapper
+      new BS(wrapper,{})
     }
   },
   mounted(){
-    this.getBannerData()
-    this.getRecommendData()
+    getBannerData().then((res)=>(
+      this.list = res.data.slider
+    ))
+    getRecommendData().then((res)=>{
+      this.recommendList = res.data.list
+    })
+    this.initBs()
     
   }
 }
 </script>
 <style lang="less" scoped>
 @import '../../style/index.less';
-.tab{
-  .w(375);
-  height: 44px;
-  display: flex;
-  div{
-    background: pink;
+.recommend{
+  .wrapper{
+    position: fixed;
+    top:88px;
+    bottom: 0px;
+    left:0px;
+    right: 0px;
+    overflow: hidden;
+    .content{
+      .w(375);
+      .title{
+        .w(375);
+        height: 65px;
+        text-align: center;
+        line-height: 65px;
+        font-size: @fs-s;
+        color:@yellow;
+      }
+      .list{
+        li{
+          .w(375);
+          height: 82px;
+          box-sizing: border-box;
+          padding: 0px 20px 20px 20px;
+          // border: 1px solid red;
+          display: flex;
+          .left{
+            width: 82px;
+            height: 60px;
+            img{ width: 60px;height:60px;}
+            // background: pink;
+          }
+          .right{
+            color: #fff;
+            font-size: @fs-s;
+          }
+        }
+      }
+    }
   }
-}  
+}
 </style>
--- vue/nz1911-music/src/main.js
+++ vue/nz1911-music/src/main.js
@@ -4,9 +4,18 @@ import './style/reset.css'
 // 引入样式重置文件
 import router from './router'
 import axios from  './utils/axios'
+import VueLazyload from  'vue-lazyload'
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios 
 // 将 axios 挂载到vue的原型上  组件继承实例,所以vue原型上的属性组件也有
+
+// 图片懒加载
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: '/favicon.ico',
+  loading: '/favicon.ico',
+  attempt: 1
+})
 new Vue({
   router,
   render: h => h(App)
