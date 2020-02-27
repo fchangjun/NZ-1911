a1dbb71c75dc3f554775e77999a8bbc53f2a1b3c
Apple <apple@AppledeMacBook-Pro.local>
2020-02-27 16:53:57 +0800

    vue06-am

--- vue/nz1911-music/src/components/Singer/index.vue
+++ vue/nz1911-music/src/components/Singer/index.vue
@@ -24,10 +24,16 @@
     </div>
     <!-- 快速列表 -->
     <div class='quicklist'>
-      <ul>
+      <ul 
+        @touchstart='touchStart'
+        @touchmove='touchMove'
+        @touchend='touchEnd'
+      >
         <li v-for="(item,index) in quickData" 
             :key="index"
+            @click='quickJump(item)'
             :class='selFindex==item?"sel":""'>
+
           {{item}}
         </li>
       </ul>
@@ -47,15 +53,24 @@
 3.渲染歌手列表 v
 4.歌手列表能滚动 v
 5.渲染快速入口 的数据 是和歌手列表数据进行关联 计算属性 v
-6.歌手列表滚动可以控制快速入口 item切换
+6.歌手列表滚动可以控制快速入口 item切换 v 
   滚动的时候 获取滚动区域内的首字母 
   1. 首字母数字 quickdata
   2. 距离数组  每一组歌手块的高度
   3. 判断向上移动的距离在距离数组里的下标 
   4. 根据下标在首字母数组里获取首字母
-7.快速入扣点击  可以控制歌手   滚动
+7.快速入扣点击  可以控制歌手   滚动  v
+  1.点击快速入口
+  2.点完了需要能滚
 8.快速入口滑动 控制歌手滚动
+  a.获取按下点的坐标
+  b.移动的过程中 计算 移动过的距离
+  c.用移动过的距离除以每一个快的尺寸 计算出滚动了多少格格
+  4.起始点的下标 +滚动的格格数  求出滚动的字母位置 
+  5. scrollElement 左边滚动到某一个元素
+
 9.点击歌手进入到歌手详情页
+
 */ 
 import BS from 'better-scroll'
 import {getSingersData} from 'api/api.js'
@@ -64,23 +79,68 @@ export default {
   data(){
     return{
       singers:[],
-      selFindex:'B',//选中的下标是什么
+      selFindex:'hot',//选中的下标是什么
     }
   },
   methods:{
-     initBs(){
-       let wrapper = this.$refs.singerWrapper
-       new BS(wrapper,{})
-       console.log(this.$refs)
-       // 获取距离数组  
-       let distance=[]
-       for (const key in this.$refs) {
-           if(key!=='singerWrapper'){
-             distance.push(this.$refs[key][0].offsetTop)
-           }
-       }
-       console.log(distance)
-     }
+    // 手指的触摸移动事件
+    touchStart(e){
+      let y = e.touches[0].pageY
+      this.touch.y=y
+      console.log('按下',y )
+    },
+    touchMove(e){
+      let moveY =e.touches[0].pageY
+      let moveDis=moveY-this.touch.y //计算出移动过的距离
+      let count =parseInt(moveDis/18) //获取移动过的格子数
+      console.log('移动',moveDis,count)
+      let moveIndex =0 + count  //获取下标
+      let moveFindex =this.quickData[moveIndex]
+      this.quickJump(moveFindex)
+    },
+    touchEnd(){
+      console.log('抬起')
+    },
+    quickJump(item){
+      // 点击每一项之后 触发事件需要知道点到谁了
+      // 根据字母获取到固定的元素
+      let dom = this.$refs[item][0]
+      // console.log('点到我了',item,dom)
+      // //  用bs 内部的方法直接跳转到固定元素
+      this.Bs.scrollToElement(dom)
+    },
+    initBs(){
+      let wrapper = this.$refs.singerWrapper
+      this.Bs = new BS(wrapper,{probeType:3})
+      // 获取距离数组  
+      let distance=[]
+      for (const key in this.$refs) {
+          if(key!=='singerWrapper'){
+            distance.push(this.$refs[key][0].offsetTop)
+          }
+      }
+    // 监听滚动
+    this.Bs.on('scroll',(pos)=>{
+      let y =Math.abs(pos.y)
+      console.log('滚了',y)
+      // 判断每一次滚动的距离在什么范围内
+      let scrollIndex = 0 //滚动所在区域的下标
+      for (let index = 0; index < distance.length; index++) {
+          if(y>=distance[index]&&y<distance[index+1]){
+            scrollIndex=index
+          }else if(y>=distance[distance.length-1]){
+          //  最后的边界判断
+            scrollIndex = distance.length-1
+          }
+      }
+        
+      // 根据下标换字母
+      let scrollFindex=this.quickData[scrollIndex]
+      console.log(scrollFindex)
+      this.selFindex=scrollFindex
+    })
+    
+    }
   },
   computed: {
     quickData(){
@@ -94,6 +154,8 @@ export default {
     }
   },
   created() {
+    // 初始化数据 把移动距离挂载到this对象下
+    this.touch={y:0}
     console.log(111)
    getSingersData().then((res)=>{
     //  处理数据
