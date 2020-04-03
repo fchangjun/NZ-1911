cc3a60f4ae4221884f22870dfc10bfcab8cd8bde
Apple <apple@AppledeMacBook-Pro.local>
2020-04-03 11:59:55 +0800

    小程序01am

---
+++ applet/demo/pages/my/my.js
@@ -0,0 +1,19 @@
+Page({
+  onLoad(){
+    //  页面重新加载的时候才会触发 tab中切换页面没有销毁 只会触发一次
+    console.log('页面加载')
+  },
+  onReady(){
+    console.log('页面渲染完成之后触发')
+  },
+  onUnload(){
+    // 页面卸载的时候会触发 缓存起来就不会触发
+    console.log('页面卸载')
+  },
+  onShow(){
+    console.log('页面显示')
+  },
+  onHide(){
+    console.log('页面隐藏')
+  }
+})
