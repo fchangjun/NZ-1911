8b55d641e536b8bcc39208d34bcf1ba12005413c
Apple <apple@AppledeMacBook-Pro.local>
2020-04-08 16:54:26 +0800

    小程序03-pm

--- applet/mall/pages/detail/detail.js
+++ applet/mall/pages/detail/detail.js
@@ -2,14 +2,16 @@ const goodsApi = require('../../api/goodsApi')
 const {config} = getApp()
 Page({
   data:{
+    pickerValue:0,
+    range:[1,2,3,4,5,6,7,8,9],
     showModal:false, //控制模态框的显示隐藏
     info:{},
     imgServer:config.imgServer
   },
   async onLoad(option){
     console.log(option)
-    let productId='5e8973abfba9d9788a2eb420'
-    // let productId=option.productId
+    // let productId='5e8973abfba9d9788a2eb420'
+    let productId=option.productId
     let {code,msg,result} = await goodsApi.getGoodsInfo(productId)
     this.setData({info:result[0]})
   },
@@ -26,9 +28,25 @@ Page({
         console.log(err)
       }
     })
-  }
+  },
+  // 切换显示模态框
+  toggleModal(){
+    let {showModal} = this.data
+    this.setData({showModal:!showModal})
+  },
+  // 数量切换
+  pickerSel(e){
+    let value = e.detail.value 
+    this.setData({pickerValue:value})
+    console.log(e)
+  },
+  //用来阻断事件的冒泡
+  son(){}
 })
 /*
 根据商品id 获取商品的数据
 根据数据渲染界面
+
+bug:
+弹出模态框应该禁止底部的滚动
 */ 
